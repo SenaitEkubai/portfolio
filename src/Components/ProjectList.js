286d012dd38453b0570e2fabfe62db314bc71326
SenaitEkubai <Senaitekubai@gmail.com>
2021-08-10 23:08:46 +0100

    technologies data

---
+++ src/Components/ProjectList.js
@@ -0,0 +1,48 @@
+import repairsforyou from "../logos/repairsforyou.png";
+import tvshow from "../logos/tvshow.png";
+import hotel1 from "../logos/hotel1.png";
+import countries from "../logos/countries.png";
+import videorecommendation from "../logos/videorecommendation.png";
+
+export const projects = [
+  {
+    name: "Repairs For You ",
+    img: repairsforyou,
+    desc:
+      "A web application made for refugees and migrants in the Coventry area to advertise and sell their home repair services ",
+    github: "https://github.com/Pat-On/repairs_for_you",
+    app: "https://repairsforyou-release.herokuapp.com/",
+  },
+  {
+    name: "Tv Shows",
+    img: tvshow,
+    desc:
+      "A web application made for refugees and migrants in the Coventry area to advertise and sell their home repair services ",
+    github: "https://github.com/Pat-On/repairs_for_you",
+    app: "https://repairsforyou-release.herokuapp.com/",
+  },
+  {
+    name: "CYF Hotel",
+    img: hotel1,
+    desc:
+      "A web application made for refugees and migrants in the Coventry area to advertise and sell their home repair services ",
+    github: "https://github.com/Pat-On/repairs_for_you",
+    app: "https://repairsforyou-release.herokuapp.com/",
+  },
+  {
+    name: "Countries",
+    img: countries,
+    desc:
+      "A web application which allows user to see lis of countries, detailed description,",
+    github: "https://github.com/Pat-On/repairs_for_you",
+    app: "https://repairsforyou-release.herokuapp.com/",
+  },
+  {
+    name: "Video recommendation",
+    img: videorecommendation,
+    desc:
+      "A web application app which allows user to see recommended youtube videos and add new videos  ",
+    github: "https://github.com/Pat-On/repairs_for_you",
+    app: "https://repairsforyou-release.herokuapp.com/",
+  },
+];
