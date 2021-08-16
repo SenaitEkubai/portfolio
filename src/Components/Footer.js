4c055a0d31d220ad3bb42279a0ccb38907c15e90
SenaitEkubai <Senaitekubai@gmail.com>
2021-08-16 18:00:40 +0100

    cleaned code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,23 +2,36 @@ import React from "react";
 import linkedin from "../logos/linkedin.svg";
 import email from "../logos/email.svg";
 import github from "../logos/github.png";
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div className="footer">
       <div>
-        <p>Senait Ekubai&copy; </p>
+        <p>Senait Ekubai&copy; 2021 </p>
       </div>
       <div>
-        <img src={linkedin} width="20" height="20" alt="linkedin logo"></img>
-        <a href="https://www.linkedin.com/in/senait-ekubai/">SenaitEkubai</a>
+        <a
+          href="https://www.linkedin.com/in/senait-ekubai/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} width="20" height="20" alt="linkedin logo"></img>
+        </a>
       </div>
       <div>
-        <img src={github} width="20" height="20" alt="github logo"></img>
-        <a href="https://github.com/SenaitEkubai">SenaitEkubai</a>
+        <a
+          href="https://github.com/SenaitEkubai"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={github} width="20" height="20" alt="github logo"></img>
+        </a>
       </div>
       <div>
-        <img src={email} width="20" height="20" alt="email log"></img>
-        <a href="https://">SenaitEkubai@gmail.com</a>
+        <Link to="/contact">
+          {" "}
+          <img src={email} width="20" height="20" alt="email log"></img>
+        </Link>
       </div>
     </div>
   );
