c04895093bf7ab2d0bc6fc2b375404f7ed18914f
SenaitEkubai <Senaitekubai@gmail.com>
2021-08-16 13:15:41 +0100

    removed introduction content

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -3,23 +3,8 @@ import React from "react";
 export default function Introduction() {
   return (
     <div className="intro">
-      <h1>A little about me </h1>
-      <p>
-        I am a full stack development graduate from code your future. I am
-        originally from Eritrea currently living in the UK. Back in Eritrea, i
-        started studying Computer engineering but i could not complete the
-        Degree due to an expected circumstances. When i moved to the Uk, I was
-        so excited to university to finish what i started but later found out it
-        was not as easy i thought it would be so i decided to teach my sellf
-        HTML and CSS. While i was in the middle of this, I found out about{" "}
-        <a href="https://codeyourfuture.io/">CodeYourFuture</a> a none profit
-        organisation which helps refugees and migrants to code and start a
-        career in the IT world.{" "}
-      </p>
-      <p>
-        I have gained very important soft and hard skills from my time in with
-        code your future.{" "}
-      </p>
+      <h1></h1>
+      <p></p>
     </div>
   );
 }
