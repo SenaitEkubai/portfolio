d1a7c9c3d5887399d4f2c3ca2fe8b55a12305ccd
SenaitEkubai <Senaitekubai@gmail.com>
2021-08-16 13:15:23 +0100

    rendered soft skills

--- src/Components/MySkills.js
+++ src/Components/MySkills.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { skills } from "./Skills";
+import { technicalSkills } from "./Skills";
+import { SoftSkills } from "./Skills";
 
 export default function MySkills() {
   return (
-    <div>
-      <h2>Technologies learned so far</h2>
+    <div className="technologies-and-skills">
+      <h2>Technologies and other skills</h2>
+      <h3>Technical Skills</h3>
       <div className="grid-container">
-        {skills.map((skill, index) => (
+        {technicalSkills.map((skill, index) => (
           <img
             key={index}
             src={skill.image}
             className="image"
             alt="images"
           ></img>
+        ))}
+      </div>
+      <h3>Other Skills</h3>
+      <div>
+        {SoftSkills.map((skill, index) => (
+          <p key={index}>{skill}</p>
         ))}
       </div>
     </div>
